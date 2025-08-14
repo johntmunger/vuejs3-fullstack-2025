caa0d44acd83f71a93b940e52fbe0126d2a174c4
johntmunger <johntmunger@gmail.com>
2025-08-14 14:51:45 -0600

    add interface for tasks validate cache

--- src/stores/loaders/tasks.ts
+++ src/stores/loaders/tasks.ts
@@ -8,7 +8,7 @@ import { defineStore } from "pinia";
 import { useMemoize } from "@vueuse/core";
 
 export const useTasksStore = defineStore('tasks-store', () => {
-    const tasks = ref<TasksWithProjects>([]);
+    const tasks = ref<TasksWithProjects | null>(null);
     const task = ref<Task | null>(null);
 
     // use memoize for caching purposes
@@ -16,15 +16,29 @@ export const useTasksStore = defineStore('tasks-store', () => {
     const loadTask = useMemoize (async (id: string) => await taskQuery(id)
     )
 
-    const validateCache = () => {
-        if (tasks.value?.length) {
-            tasksWithProjectsQuery.then(({ data, error }) => {
-                if (JSON.stringify(tasks.value) === JSON.stringify(data)) {
+    interface ValidateCacheParams {
+        ref: typeof tasks | typeof task
+        query: typeof tasksWithProjectsQuery | typeof taskQuery
+        key: string
+        loaderFn: typeof loadTasks | typeof loadTask
+    }
+
+    const validateCache = ({
+        ref,
+        query,
+        key,
+        loaderFn
+    }: ValidateCacheParams) => {
+        if (ref.value) {
+            const finalQuery = typeof query === 'function' ? query(key) : query
+
+            finalQuery.then(({ data, error }) => {
+                if (JSON.stringify(ref.value) === JSON.stringify(data)) {
                     return
                 }
                 else {
-                    loadTasks.delete('tasks')
-                    if (!error && data) tasks.value = data;
+                    loaderFn.delete(key)
+                    if (!error && data) ref.value = data;
                 }
             })
         }
@@ -39,7 +53,12 @@ export const useTasksStore = defineStore('tasks-store', () => {
 
     if (data) tasks.value = data;
 
-    validateCache();
+        validateCache({
+            ref: tasks,
+            query: tasksWithProjectsQuery,
+            key: 'tasks',
+            loaderFn: loadTasks
+        });
     };
 
     const getTask = async (id: string) => {
@@ -51,7 +70,12 @@ export const useTasksStore = defineStore('tasks-store', () => {
 
         if (data) task.value = data
 
-        validateCache();
+        validateCache({
+            ref: task,
+            query: taskQuery,
+            key: id,
+            loaderFn: loadTask
+        });
     }
 
     return {
