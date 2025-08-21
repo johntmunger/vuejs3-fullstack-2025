bac42d7dacf0f7e26b5f93a136aa6daa96089ec8
johntmunger <johntmunger@gmail.com>
2025-08-21 12:29:04 -0600

    add dynamic collapsing and open for sidebar menu

--- src/components/Layout/Sidebar.vue
+++ src/components/Layout/Sidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useMenu } from "@/composables/menu";
 import SidebarLinks from "./SidebarLinks.vue";
 import { Button } from "@/components/ui/button";
 
@@ -49,16 +50,19 @@ const executeAction = async (linkTitle: string) => {
 };
 
 defineEmits(['taskClicked'])
+
+const {menuOpen, toggleMenu} = useMenu()
 </script>
 
 <template>
   <aside
-    class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 lg:w-52 w-16 transition-[width]"
+    class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 transition-[width]"
+    :class="{'w-52': menuOpen, 'w-24': !menuOpen}"
   >
     <div
       class="flex h-16 items-center border-b px-2 lg:px-4 shrink-0 gap-1 justify-between"
     >
-      <Button variant="outline" size="icon" class="w-8 h-8">
+      <Button @click="toggleMenu" variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:menu"></iconify-icon>
       </Button>
 
---
+++ src/composables/menu.ts
@@ -0,0 +1,10 @@
+export const menuOpen = ref(false)
+
+export const useMenu = () => {
+    const toggleMenu = () => (menuOpen.value = !menuOpen.value)
+
+    return {
+        menuOpen,
+        toggleMenu
+    }
+}
